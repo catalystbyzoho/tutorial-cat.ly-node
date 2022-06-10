216a8a77833ab8b12ed48cfb53cf28073272c3a2
prasanna.ga <prasanna.ga@zohocorp.com>
2022-06-10 17:03:46 +0530

    removed segment id

--- functions/catly/utils/constants.js
+++ functions/catly/utils/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
 	table_name: 'UrlShorten',
-	short_url_segment: '1434000000210780', // your segment id
+	short_url_segment: '<segment-id>', // your cache segment id here
 	errorPage: '/app/error.html'
 });
